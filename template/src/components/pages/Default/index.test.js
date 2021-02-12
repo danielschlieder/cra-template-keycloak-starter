0a2f704bc3b80f31be0f73953aa63e2824858380
dasc <info@schlieder.de>
2021-02-12 14:53:15 +0100

    Mocking config fetching

--- template/src/components/pages/Default/index.test.js
+++ template/src/components/pages/Default/index.test.js
@@ -4,7 +4,23 @@ import Page from "./index";
 import PreLoader from "../../molecules/Preloader";
 import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "../../../keycloak";
-
+import config from "../../../../public/config.json";
+async function mockFetch(url) {
+  switch (url) {
+    case "config.json": {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => config,
+      };
+    }
+    default: {
+      throw new Error(`Unhandled request: ${url}`);
+    }
+  }
+}
+beforeAll(() => jest.spyOn(window, "fetch"));
+beforeEach(() => window.fetch.mockImplementation(mockFetch));
 describe("Comp without props", () => {
   it("should render without props", async () => {
     const { queryByText } = render(
